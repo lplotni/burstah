f5aaee14a5b978dcfe5325d7d436ac4790146f5b
Lukasz Plotnicki <lplotnicki@thoughtworks.com>
2014-07-13 20:29:34 +0200

    Yet again / look at previous msg.

--- config.js
+++ config.js
@@ -3,22 +3,22 @@ var config = {
     port: '8153',
     auth: '', //user:password
     project: {
-    name: 'QEN',
-      stages: [
-      'build :: both',
-      'integration-test :: backend-integration',
-      'deploy-dev :: backend',
-      'deploy-dev :: client',
-      'smoke-test-dev :: backend',
-      'functional-test :: both',
-      'deploy-showcase :: backend',
-      'deploy-showcase :: client',
-      'smoke-test-showcase :: backend',
-      'deploy-integration :: backend',
-      'deploy-integration :: client',
-      'smoke-test-integration :: backend'
-    ]
-  }
+      name: 'QEN',
+        stages: [
+        'build :: both',
+        'integration-test :: backend-integration',
+        'deploy-dev :: backend',
+        'deploy-dev :: client',
+        'smoke-test-dev :: backend',
+        'functional-test :: both',
+        'deploy-showcase :: backend',
+        'deploy-showcase :: client',
+        'smoke-test-showcase :: backend',
+        'deploy-integration :: backend',
+        'deploy-integration :: client',
+        'smoke-test-integration :: backend'
+      ]
+    }
   };
 
 exports.getConfig = function(){return config;};
